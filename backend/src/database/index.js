894360782a78a32b6f4f72d2b0824b5720669b4e
NaluFigueira <anachfigueira@gmail.com>
2019-12-08 17:32:08 -0300

    Configurando o sequelize

---
+++ backend/src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import DataBaseConfig from '../config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(DataBaseConfig);
+
+    models.map(model => model.init(this.connection));
+
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
+  }
+}
+
+export default new Database();
